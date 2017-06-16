604f5d90a00620df45a239cadec5b15f6408e146
dhingratul <atul.dhingra@rutgers.edu>
2017-06-16 16:00:47 -0400

    Run Program: Comments

--- RunTextureRecognition.m
+++ RunTextureRecognition.m
@@ -1,3 +1,5 @@
+%% Run Texture Recongition Experiment
+% Synthesize and Grow Texture
 addpath('/home/dhingratul/Dropbox/Academics/Spring2016/CS-534/HW/HW2/Data');
 tic;
 WindowSize=5;
@@ -8,7 +10,7 @@ if(r>100 && c>100)
     SampleImage=imresize(SampleImage,[50,50]);
     SampleImage=double(SampleImage)+0.001;
 else
-SampleImage=imresize(SampleImage,[50,50]);
+    SampleImage=imresize(SampleImage,[50,50]);
 end
 r=floor(r/2); r=r+70;
 c=floor(c/2); c=c+70;
@@ -20,6 +22,6 @@ for i=r:r+size(SampleImage,1)-1
 end
 %Set last parameter to 0 if you don't want the texture being displayed
 SampleImage=double(SampleImage);
-OutImage=Synthesize(SampleImage,Image,WindowSize,0); 
+OutImage=Synthesize(SampleImage,Image,WindowSize,0);
 time=toc;
-% OutImage=GrowImage(SampleImage,Image,WindowSize);
+% OutImage=GrowImage(SampleImage,Image,WindowSize,1);
